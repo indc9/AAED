2a5d1ec2a1b68b65bef9f933a640d3132733f73d
indc9 <indc94@gmail.com>
2016-06-20 17:25:53 +0200

    Add files via upload

--- TAD CONSULTORIO/consultorio.h
+++ TAD CONSULTORIO/consultorio.h
@@ -3,7 +3,7 @@
 
 struct Medico{
 	int cod_med;
-	Cola<Paciente> p;
+	Cola<Paciente> pac;
 };
 
 struct Paciente{
@@ -14,11 +14,84 @@ struct Paciente{
 class Consultorio{
 public:
 	Consultorio();
+	bool existe_med(const Medico& m);
 	void alta_medico(const Medico& m);
-	void annadir_paciente(const Paciente& paciente);
+	void annadir_paciente(const Paciente& paciente, const Medico& m);
 	Paciente turno_paciente(const Medico& m);
 	void atender(const Paciente& p);
 	bool cola_pacientes(const Medico& m);
+	~Consultorio();
 private:
 	Lista<Medico> l;
+	typename Lista<Medico>::posicion pos_med = -1;
 };
+
+
+
+inline Consultorio::Consultorio(){}
+
+inline bool Consultorio::existe_med(const Medico& m)
+{
+	typename Lista<Medico>::posicion p = l.primera();
+	bool encontrado = false;
+
+	while(p != l.fin())
+	{
+		if(l.elemento(p).cod_med == m.cod_med && !encontrado)
+		{
+			encontrado = true;
+		}	
+		p = l.siguiente(p);
+	}
+	if(encontrado)
+		pos_med = p;
+
+	return encontrado;
+}
+
+inline void Consultorio::alta_medico(const Medico& m)
+{
+	if(!existe_med(m))
+		l.insertar(m, l.fin());	
+}
+
+inline void Consultorio::annadir_paciente(const Paciente& paciente, const Medico& m)
+{
+	typename Cola<Paciente>::posicion x = l.elemento(p).pac.primera();
+
+	if(existe_med(m))
+		l.elemento(pos_med).pac.push(paciente));		
+}
+
+inline bool Consultorio::cola_pacientes(const Medico& m)
+{
+	bool existe = false;
+
+	if(existe_med(m))
+		if(!l.elemento(pos_med).pac.vacia())
+			existe = true;
+
+	return existe;
+}
+
+inline Paciente Consultorio::turno_paciente(const Medico& m)
+{	
+	if(existe_med(m))
+		if(cola_pacientes(m))		
+			return l.elemento(pos_med).pac.frente();
+}
+
+inline void Consultorio::atender(const Medico& m)
+{
+	Paciente p;
+
+	if(existe_med(m))
+		p = turno_paciente(m);
+
+	if(p == l.elemento(pos_med).pac.frente())
+		l.elemento(pos_med).pac.pop();
+}
+
+
+
+Consultorio::~Consultorio(){}
